aa7c64a9950807a05dc54db8f89f75d4dc872947
Eldor <eldortirkashev@gmail.com>
2020-12-02 15:58:00 +0500

    first commit

---
+++ lesson1.py
@@ -0,0 +1,52 @@
+# o'zgaruvchilar (data types)
+# integer - int butun son
+# float - haqiqiy son
+# list - massiv
+# dictionary - lug'atlar
+# boolean - mantiqiy tur
+# string - matn turi
+
+
+# o'zgaruvchi e'loni
+# a = 12 # int
+# b = 15.2 # float
+# c = "nimadir" #string
+# boolean = True # bool
+# list = [1,2,'njnjsan'] #massiv list
+# dictionary = {'key':'value'} # dict
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(list))
+# print(type(dictionary))
+#
+"""
+Katta tekst kommentariya
+"""
+'''
+nimadir tekst
+'''
+import math
+# a = int(input('a: '))
+# print("Kvadrat perimetri:",4*a)
+# print("Kvadrat yuzasi:", a**2)
+# print(2**100000)
+# c = 189387129
+# d = 13
+# print(c/d)
+# print(c//d) # butun qismi
+# print(13%5) # qoldiq qismini olish
+# print(math.sqrt(c))# ildiz olish
+# list = [8,5,6,1,2,3,4,4,5,5,6,5,88]
+# print(min(list))
+# print(max(list))
+# pi = 3.14
+# d = float(input('d:'))
+# print('L:', d*pi)
+# print(abs(d))
+
+
+
+
+
+
